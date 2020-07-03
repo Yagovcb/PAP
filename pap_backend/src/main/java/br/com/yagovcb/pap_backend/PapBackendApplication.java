9cafbf09da5b262030170b50b236d44f55c0e6a5
Yago do Valle Castelo Branco <desenvolvimento@emagesolucoes.com.br>
2020-07-03 19:53:22 -0300

    Initial commit

---
+++ pap_backend/src/main/java/br/com/yagovcb/pap_backend/PapBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.yagovcb.pap_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PapBackendApplication.class, args);
+    }
+
+}
